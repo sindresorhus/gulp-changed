f86ced48127f2ae9c60b9430b39c39a2fa454f96
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-02-13 19:54:25 -0800

    Adds tests.

--- test.js
+++ test.js
@@ -6,45 +6,60 @@ var gutil = require('gulp-util');
 var rimraf = require('rimraf');
 var changed = require('./index');
 
-it('should only pass through changed files', function (cb) {
-	var stream = changed('tmp');
-	var files = [];
+function test(opts) {
+	var desc = 'should only pass through changed files';
+	var extension = '.js';
 
-	try {
-		fs.mkdirSync('tmp');
-		fs.writeFileSync(path.join('tmp', 'foo.js'), '');
-	} catch (err) {}
+	if (opts && opts.extension) {
+		desc += ' using extension ' + opts.extension;
+		extension = opts.extension;
+	} else {
+		desc += ' using file extension';
+	}
 
-	stream.on('data', function (file) {
-		files.push(file);
-		fs.writeFileSync(path.join('tmp', file.relative), file);
+	it(desc, function (cb) {
+		var stream = changed('tmp', opts);
+		var files = [];
+
+		try {
+			fs.mkdirSync('tmp');
+			fs.writeFileSync(path.join('tmp', 'foo' + extension), '');
+		} catch (err) {}
+
+		stream.on('data', function (file) {
+			files.push(file);
+			fs.writeFileSync(path.join('tmp', file.relative), file);
+		});
+
+		stream.on('end', function () {
+			assert.equal(files.length, 1);
+			assert.equal(files[0].relative, 'bar.js');
+			rimraf.sync('tmp');
+			cb();
+		});
+
+		stream.write(new gutil.File({
+			cwd: __dirname,
+			base: __dirname,
+			path: 'foo.js',
+			contents: new Buffer(''),
+			stat: {
+				mtime: fs.statSync(path.join('tmp', 'foo' + extension))
+			}
+		}));
+
+		stream.write(new gutil.File({
+			base: __dirname,
+			path: 'bar.js',
+			contents: new Buffer(''),
+			stat: {
+				mtime: new Date()
+			}
+		}));
+
+		stream.end();
 	});
+}
 
-	stream.on('end', function () {
-		assert.equal(files.length, 1);
-		assert.equal(files[0].relative, 'bar.js');
-		rimraf.sync('tmp');
-		cb();
-	});
-
-	stream.write(new gutil.File({
-		cwd: __dirname,
-		base: __dirname,
-		path: 'foo.js',
-		contents: new Buffer(''),
-		stat: {
-			mtime: fs.statSync(path.join('tmp', 'foo.js'))
-		}
-	}));
-
-	stream.write(new gutil.File({
-		base: __dirname,
-		path: 'bar.js',
-		contents: new Buffer(''),
-		stat: {
-			mtime: new Date()
-		}
-	}));
-
-	stream.end();
-});
+test();
+test({extension: '.coffee'});
