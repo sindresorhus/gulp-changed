7a2bcb26aacc53f196843c905354f810055253e4
Richard Fussenegger <richard@fussenegger.info>
2015-02-07 14:19:20 +0100

    JSDoc for all functions.

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 'use strict';
 
-var pluginName = 'gulp-changed';
-
 var fs = require('fs');
 var path = require('path');
 var crypto = require('crypto');
 var gutil = require('gulp-util');
 var through = require('through2');
 
-// ignore missing file error
+/**
+ * Name of this gulp plugin.
+ *
+ * @type {string}
+ */
+var pluginName = 'gulp-changed';
+
+/**
+ * Evaluate error returned by node's file system methods, missing file error is ignored and the source file is pushed.
+ *
+ * @param {Stream} stream - Through stream object.
+ * @param {File} sourceFile - Vinyl file object.
+ * @param {null|Error} err - Possibly thrown error.
+ * @return {null|Error} The `err` argument.
+ */
 function fsOperationFailed(stream, sourceFile, err) {
 	if (err) {
 		if (err.code !== 'ENOENT') {
@@ -23,11 +35,25 @@ function fsOperationFailed(stream, sourceFile, err) {
 	return err;
 }
 
+/**
+ * Get the sha1 hex digest of a buffer.
+ *
+ * @param {Buffer} buf - The buffer to get the sha1 hex digest for.
+ * @return {string} The sha1 hex digest of the buffer.
+ */
 function sha1(buf) {
 	return crypto.createHash('sha1').update(buf).digest('hex');
 }
 
-// only push through files changed more recently than the destination files
+/**
+ * Compare last modified time of source and target and only push through files which changed.
+ *
+ * @param {Stream} stream - Through stream object.
+ * @param {Function} cb - Done callback.
+ * @param {File} sourceFile - Vinyl file object.
+ * @param {string} targetPath - Path of the target.
+ * @return {undefined}
+ */
 function compareLastModifiedTime(stream, cb, sourceFile, targetPath) {
 	fs.stat(targetPath, function (err, targetStat) {
 		if (!fsOperationFailed(stream, sourceFile, err)) {
@@ -40,7 +66,15 @@ function compareLastModifiedTime(stream, cb, sourceFile, targetPath) {
 	});
 }
 
-// only push through files with different SHA1 than the destination files
+/**
+ * Compare sha1 hex digest of source and target and only push through files which changed.
+ *
+ * @param {Stream} stream - Through stream object.
+ * @param {Function} cb - Done callback.
+ * @param {File} sourceFile - Vinyl file object.
+ * @param {string} targetPath - Path of the target.
+ * @return {undefined}
+ */
 function compareSha1Digest(stream, cb, sourceFile, targetPath) {
 	fs.readFile(targetPath, function (err, targetData) {
 		if (!fsOperationFailed(stream, sourceFile, err)) {
@@ -56,6 +90,23 @@ function compareSha1Digest(stream, cb, sourceFile, targetPath) {
 	});
 }
 
+/**
+ * Invocation method of the plugin.
+ *
+ * @param {Function|string} dest - The destination of the file, this can be one of:
+ *   1. A string which contains the same value as `gulp.dest` of this pipe receives.
+ *   2. A function which will get the through file object as first argument and has to return a string like 1. of this
+ *      list.
+ * @param {{
+ *   cwd: string
+ *   extension: string
+ *   hasChanged: Function
+ * }} [opts] - Plugin options:
+ *   - `cwd` is the current working directory to resolve paths, defaults to `process.cwd`.
+ *   - `extension` can be used to replace the source's extension if the destination file has a different one.
+ *   - `hasChanged` is the function to determine if a file has changed, defaults to `compareLastModified` of this plugin.
+ * @return {undefined}
+ */
 module.exports = function (dest, opts) {
 	if (!dest) {
 		throw new gutil.PluginError(pluginName, '`dest` required');
