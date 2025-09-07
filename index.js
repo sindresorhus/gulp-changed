3b534f35a70892ba3dbd1e0ce69255bd2bac974b
Sindre Sorhus <sindresorhus@gmail.com>
2025-09-08 02:23:15 +0700

    Fix `compareLastModifiedTime` on Windows
    Fixes #49

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ import {gulpPlugin} from 'gulp-plugin-extras';
 
 // Only push through files changed more recently than the destination files
 export async function compareLastModifiedTime(sourceFile, targetPath) {
+	if (!sourceFile.stat) {
+		return;
+	}
+
 	const targetStat = await fs.stat(targetPath);
 
 	// TODO: This can be removed when Gulp supports mtime as bigint.
 	// `fs.stat(targetPath, {bigint: true})`
 	/*
-	Precision is lost in the `mtime` when Gulp copies the file from source to target so we cannot compare the modified times directly. This has been the case since Gulp 4. Now, due to an issue in libuv affecting Node.js 14.17.0 and above (including 16.x: https://github.com/nodejs/node/issues/38981) when Gulp copies the file to the target, its `mtime` may be behind the source file by up to 1ms. For example, if the source file has a `mtime` like `1623259049896.314`, the target file `mtime` can end up as `1623259049895.999`. So to compare safely we use floor on the source and ceil on the target, which would give us `1623259049896` for both source and target in that example case.
+	Precision is lost in the `mtime` when Gulp copies the file from source to target so we cannot compare the modified times directly. This has been the case since Gulp 4.
+
+	On non-Windows systems:
+	Due to an issue in libuv affecting Node.js 14.17.0 and above (https://github.com/nodejs/node/issues/38981), when Gulp copies the file to the target, its `mtime` may be behind the source file by up to 1ms. For example, if the source file has a `mtime` like `1623259049896.314`, the target file `mtime` can end up as `1623259049895.999`. To compare safely we use floor on the source and ceil on the target, which would give us `1623259049896` for both source and target in that example case.
+
+	On Windows:
+	File modification times are often rounded to the nearest second. For example, a source file with `mtime` of `1446818873134` gets copied to a target with `mtime` of `1446818873000`. Both represent the same second, but comparing at millisecond precision would incorrectly detect them as different. Therefore, we compare at second precision by dividing by 1000 and flooring both values.
 	*/
-	if (sourceFile.stat && Math.floor(sourceFile.stat.mtimeMs) > Math.ceil(targetStat.mtimeMs)) {
+
+	if (process.platform === 'win32') {
+		// Compare at second precision on Windows to handle filesystem second-rounding
+		if (Math.floor(sourceFile.stat.mtimeMs / 1000) > Math.floor(targetStat.mtimeMs / 1000)) {
+			return sourceFile;
+		}
+	} else if (Math.floor(sourceFile.stat.mtimeMs) > Math.ceil(targetStat.mtimeMs)) {
+		// Use millisecond precision with floor/ceil for sub-second precision issues
 		return sourceFile;
 	}
 }
